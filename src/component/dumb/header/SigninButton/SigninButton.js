2e529df4f5ef9029a715f04826b4e9f340900a2a
Lavanyak01 <102778119+Lavanyak01@users.noreply.github.com>
2022-10-03 15:40:09 +0530

    Update SigninButton.js

--- src/component/dumb/header/SigninButton/SigninButton.js
+++ src/component/dumb/header/SigninButton/SigninButton.js
@@ -3,7 +3,7 @@ import './button.css';
 function SigninButton(){
     return (
         <div>
-            <a className='signin-button' href="/login">Sign In</a>
+            <a className='signin-button' href="#">Sign In</a>
         </div>
     );
 }
