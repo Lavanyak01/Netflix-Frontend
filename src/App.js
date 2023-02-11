a26f5855299929743d7d8ca300a1ef553dc8b6cd
Lavanyak01 <102778119+Lavanyak01@users.noreply.github.com>
2023-02-11 11:06:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from './Pages/HomePage';
+import SignupPage from './Pages/SignupPage';
+import SignInPage from './Pages/SignInPage';
 
 
 
 function App() {
   return (
-    <>
-      <HomePage />
-    
-    </>
+     <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route  path="/signup" element={<SignupPage />} />
+        <Route  path="/login" element={<SignInPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
